561fddd68a2980ff6f5fc5717649c95e5d2443a7
timoo <90513578+Tommie1236@users.noreply.github.com>
2025-05-26 01:51:22 +0200

    updated dotfiles. don't know what exactly changed

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+DOTFILES="$HOME/.dotfiles"
 
-ln -s ~/.dotfiles/.config/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -snf $DOTFILES/.config/hypr ~/.config/hypr
+ln -snf $DOTFILES/.config/mako ~/.config/mako
+ln -snf $DOTFILES/.config/waybar ~/.config/waybar
+ln -snf $DOTFILES/.config/nvim ~/.config/nvim
+ln -snf $DOTFILES/.config/fish ~/.config/fish
+ln -snf $DOTFILES/.config/ly ~/.config/ly
+
+sudo ln -snf $DOTFILES/.config/ly /etc/ly
+
+echo "skipped systemd services. link manually if needed"
+# sudo ln -snf $DOTFILES/systemd/user/ly@tty2.service /etc/systemd/system/
+# sudo ln -snf $DOTFILES/systemd/user/ly@tty3.service /etc/systemd/system/

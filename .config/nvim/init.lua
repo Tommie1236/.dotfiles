8290c660b7af08f3df21611fcde341c19a8096e9
timoo <90513578+Tommie1236@users.noreply.github.com>
2025-05-26 02:13:35 +0200

    moved nvim config from init.vim to lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,43 @@
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.number = true
+
+vim.api.nvim_create_augroup("numbertoggle", { clear = true })
+
+vim.api.nvim_create_autocmd(
+    { "BufEnter", "FocusGained", "InsertLeave", "WinEnter" },
+    {
+        group = "numbertoggle",
+	pattern = "*",
+	callback = function()
+            if vim.wo.number and vim.fn.mode() ~= "i" then
+                vim.wo.relativenumber = true
+            end
+	end,
+    }
+)
+
+vim.api.nvim_create_autocmd(
+	{ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" },
+	{
+		group = "numbertoggle",
+		pattern = "*",
+		callback = function()
+			if vim.wo.number then
+				vim.wo.relativenumber = false
+			end
+		end,
+	}
+)
+
+
+
+
+
+
+require("config.lazy")
+
+

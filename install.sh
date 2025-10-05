985f4b0cb95001fff96864f7c156f39623e54c23
timoo <90513578+Tommie1236@users.noreply.github.com>
2025-10-05 02:34:23 +0200

    added keyd and first key remap

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ ln -snf $DOTFILES/.config/waybar ~/.config/waybar
 ln -snf $DOTFILES/.config/nvim ~/.config/nvim
 ln -snf $DOTFILES/.config/fish ~/.config/fish
 ln -snf $DOTFILES/.config/ly ~/.config/ly
+sudo ln -snf $DOTFILES/.config/keyd/keyd /etc/keyd
 
 sudo ln -snf $DOTFILES/.config/ly /etc/ly
 
